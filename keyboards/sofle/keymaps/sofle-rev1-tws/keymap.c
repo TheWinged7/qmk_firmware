3e5f64622add3af37769ab22042d6d0c766daa40
TheWinged7 <thewinged7@hotmail.com>
2022-08-05 17:42:21 +1000

    Update sofle layout

--- keyboards/sofle/keymaps/sofle-rev1-tws/keymap.c
+++ keyboards/sofle/keymaps/sofle-rev1-tws/keymap.c
@@ -62,44 +62,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //  Layer 0 - _base
     [_Base] = LAYOUT( \
     // ,-----------------------------------------.                    ,-----------------------------------------.
-    // | ` ~  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Game |
-        KC_ESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                     KC_6,  KC_7,  KC_8,  KC_9,   KC_0, TG(2), \
+    // |Escape|   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Game |
+        KC_ESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                     KC_6,  KC_7,  KC_8,  KC_9,   KC_0,  KC_NO, \
     // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
     // | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |  UP  |   O  |   P  |  ' " |
-        KC_TAB, KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,                       KC_Y,  KC_U,  KC_I,  KC_O,  KC_P, KC_QUOT, \
+        KC_TAB, KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,                      KC_Y,  KC_U,  KC_I,  KC_O,  KC_P, KC_QUOT, \
     // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
     // |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |  ; : | Enter|
         KC_LSFT, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                      KC_H,  KC_J, KC_K,  KC_L, KC_SCLN, KC_ENT, \
     // |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-    // |LCTR|   Z  |   X  |   C  |   V  |   B    |-------|    |-------|   N  |   M  | , <  | . >  |  / ? |      |
+    // |LCTR  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  | , <  | . >  |  / ? |      |
         KC_LCTL, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_NO,      KC_NO, KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_NO, \
     // `-----------------------------------------/       /     \      \-----------------------------------------'
-    //            |      |      | ALT  |  Win | /Space  /       \Leader\  | Bspc |Arrows| RAlt | RCTR |
-    //            |      |      |      |      |/       /         \      \ |      | mode |      |      |
-                KC_NO, KC_NO, KC_LALT, KC_RGUI, KC_SPC,           KC_LEAD, KC_BSPC, OSL(1), KC_RALT, KC_RCTL \
+    //            |      |      | ALT  |  Win | /Space  /       \ Bspc \  |Arrows|Leader| RAlt | RCTR |
+    //            |      |      |      |      |/       /         \      \ | mode |      |      |      |
+                KC_NO, KC_NO, KC_LALT, KC_RGUI, KC_SPC,           KC_BSPC, OSL(1), KC_LEAD, KC_RALT, KC_RCTL \
     //            `----------------------------------'           '------''---------------------------'
-
         ),
+
     // Layer 1 - _Arrows
     [_Arrows] = LAYOUT( \
     // ,-----------------------------------------.                    ,-----------------------------------------.
-    // | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | Game |
-        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS, \
+    // | Game |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+         TG(2), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, \
     // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    // | Tab  |      | Psc  | Home |      |      |                    | Home |      |  UP  |  F11 | F12  | ` ~  |
-       KC_TRNS, KC_NO, KC_PSCR, KC_HOME, KC_PGUP, KC_NO,              KC_HOME, KC_NO, KC_UP, KC_F11, KC_F12, KC_GRV, \
+    // | Tab  |   Q  | Psc  |   E  |   R  |   T  |                    | Home |      |  UP  |      | F12  | ` ~  |
+        KC_TAB, KC_Q, KC_PSCR, KC_E,  KC_R,  KC_T,                    KC_HOME, KC_NO, KC_UP, KC_NO, KC_F12, KC_GRV, \
     // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    // |LShift| Del  | End  |      |      |      |-------.    ,-------| End  | LEFT | DOWN | RIGHT|      | Enter|
-        KC_TRNS, KC_NO, KC_DEL, KC_END, KC_PGDN, KC_NO,              KC_END, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_TRNS, \
+    // |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------| End  | LEFT | DOWN | RIGHT|      | Enter|
+        KC_LSFT, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_END, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_TRNS, \
     // |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-    // |      |      |      |      |      |      _-------|    |-------|  - _ |  = + | [ {  | ] }  |  \ | |      |
-     KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO, \
+    // |LCTR  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|  - _ |  = + | [ {  | ] }  |  \ | |      |
+        KC_LCTL, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_NO,      KC_NO, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO, \
     // `-----------------------------------------/       /     \      \-----------------------------------------'
-    //            |      |      | ALT  |  Win | /Space  /       \Leader\  |  Del |Arrows| RAlt | RCTR |
-    //            |      |      |      |      |/       /         \      \ |      | mode |      |      |
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS \
+    //            |      |      | ALT  |  Win | /Space  /       \ Bspc \  |Arrows|Leader| RAlt | RCTR |
+    //            |      |      |      |      |/       /         \      \ | mode |      |      |      |
+                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
     //            `----------------------------------'           '------''---------------------------'
                     ),
+
     /* Layer 2
      * ,-----------------------------------------.                    ,-----------------------------------------.
      * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
